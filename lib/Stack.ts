f13c31944cf590fa8e61bbe208d933f7e6ac1f98
Philipp Alferov <philipp.alferov@gmail.com>
2017-07-31 22:50:55 -0400

    Use linked list for stack implementation

--- lib/Stack.ts
+++ lib/Stack.ts
@@ -1,21 +1,49 @@
-export default class Stack<T> {
-  private items: T[] = [];
-  private size: number = 0;
-  public pop(): T | void {
-    this.size = this.size - 1;
-    return this.items.pop();
+export class StackNode<T> {
+  public next: StackNode<T> | void;
+  public data: T;
+  constructor(data: T) {
+    this.data = data;
   }
-  public push(node: T): number {
+}
+
+export default class Stack<T> {
+  private size: number = 0;
+  private top: StackNode<T> | void;
+
+  public pop(): T | void {
+    if (!this.top) return;
+
+    const node = this.top;
+    this.top = node.next;
+    this.size = this.size - 1;
+
+    return node.data;
+  }
+
+  public push(data: T): number {
+    const newNode = new StackNode(data);
+
+    if (!this.top) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+
     this.size = this.size + 1;
-    this.items.push(node);
     return this.size;
   }
-  public peek(): T {
-    return this.items[this.size - 1];
+
+  public peek(): T | void {
+    if (!this.top) return;
+
+    return this.top.data;
   }
+
   public isEmpty(): boolean {
     return this.size === 0;
   }
+
   public getSize(): number {
     return this.size;
   }
